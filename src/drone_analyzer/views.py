1af35c9546962e6f447dcb9fc9e646470967605c
jholaj <thezmrdus@gmail.com>
2024-02-28 21:24:27 +0100

    basic frontend + edited routing

--- src/drone_analyzer/views.py
+++ src/drone_analyzer/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage_view(request):
+    if request.method == "POST":
+        telemetry_data = request.FILES.get('telemetry_data')
+        status_data = request.FILES.get('status_data')
+
+        context = {
+           
+        }
+
+        return render(request, "index.html", context)
+
+    if request.method == "GET":
+        context = {
+
+        }
+
+        return render(request, "index.html", context)
--- src/dronesite/urls.py
+++ src/dronesite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from drone_analyzer.views import homepage_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("homepage", homepage_view, name="homepage")
 ]
