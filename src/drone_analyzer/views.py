f0a409d7347f81f159921cc725d91c58a388e2cb
jholaj <thezmrdus@gmail.com>
2024-03-03 23:31:10 +0100

    Loading data from backend, Map & Slider + Bubble

--- src/drone_analyzer/views.py
+++ src/drone_analyzer/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import csv
 import io
 import json
 
 ##################################
 ##### AUTHOR: JAROSLAV HOLAJ #####
-#####   TIME SPENT: 3h 30m   #####
+#####   TIME SPENT: 7h 30m   #####
 ##################################
 
 # Create your views here.
+@csrf_exempt
 def homepage_view(request):
     if request.method == "POST":
         telemetry_form = request.FILES.get('telemetry_data')
         status_form = request.FILES.get('status_data')
 
-        telemetry_data = load_csv(telemetry_form)
-        status_data = load_csv(status_form)
+        if not any([telemetry_form, status_form]):
+            return JsonResponse({'error': "Please upload both telemetry and status data."}, status=400)
+
+        try:
+            telemetry_data = load_csv(telemetry_form)
+            status_data = load_csv(status_form)
+
+            longitude_latitude_data = [{'longitude': entry['longitude'], 'latitude': entry['latitude'], 'time': entry['time']} for entry in telemetry_data]
+            
+            return JsonResponse({'longitude_latitude_data': longitude_latitude_data})
+        except Exception as e:
+            return JsonResponse({'error': f"Error processing data: {str(e)}"}, status=500)
+
+    return render(request, "index.html", {})
         ########## what to measure ###########
         ## add map
         ### time
@@ -43,13 +58,6 @@ def homepage_view(request):
         ### quality of signal (x - time, y - RSRQ)
         ### noise ratio (x - time, y - SNR)
 
-
-        context = {
-           
-        }
-
-        return render(request, "index.html", context)
-
     if request.method == "GET":
         context = {
 
@@ -63,15 +71,6 @@ def load_csv(csv_file):
 
         data_reader = csv.DictReader(io.StringIO(csv_text))
         
-        data = []
-        
-        for row in data_reader:
-            # all columns
-            data_row = {}
-            for column, value in row.items():
-                data_row[column] = value
-            data.append(data_row)
+        data = [row for row in data_reader]
 
-        json_data = json.dumps(data)
-
-        return json_data
+        return data
---
+++ src/static/map.js
@@ -0,0 +1,146 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mapBtn').addEventListener('click', function() {
+        fetch('homepage', {
+            method: 'POST',
+            body: new FormData(document.querySelector('form'))
+        })
+        .then(response => response.json())
+        .then(data => {
+            createMap(data.longitude_latitude_data);
+        })
+        .catch(error => {
+            console.error('Error when loading data:', error);
+        });
+    });
+});
+
+function createMap(data){
+    if (data && data.length > 0 && data[0].longitude && data[0].latitude) {
+        const map = new ol.Map({
+            target: 'mapGraph',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM({
+                        attributions: []
+                    })
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([data[0].longitude, data[0].latitude]),
+                zoom: 16,
+                maxZoom: 20
+            })
+        });
+
+        const features = data.map(entry => {
+            const point = ol.proj.fromLonLat([entry.longitude, entry.latitude]);
+            const feature = new ol.Feature({
+                geometry: new ol.geom.Point(point),
+                time: entry.time
+            });
+            return feature;
+        });
+
+        const source = new ol.source.Vector({
+            features: features
+        });
+
+        const layer = new ol.layer.Vector({
+            source: source,
+            style: function(feature) {
+                if (feature === selectedFeature) {
+                    return new ol.style.Style({
+                        image: new ol.style.Circle({
+                            radius: 6,
+                            fill: new ol.style.Fill({ color: 'red' }),  // if selected => red
+                            stroke: new ol.style.Stroke({ color: 'black', width: 1 })
+                        })
+                    });
+                } else {
+                    return new ol.style.Style({
+                        image: new ol.style.Circle({
+                            radius: 4,
+                            stroke: new ol.style.Stroke({ color: 'blue', width: 1 }), // default blue
+                        })
+                    });
+                }
+            }
+        });
+
+        map.addLayer(layer);
+
+        let selectedFeature = null;
+
+
+        // init slider container
+        const timeSliderContainer = document.createElement('div');
+        timeSliderContainer.className = 'slider-container';
+        // init slider
+        const timeSlider = document.createElement('input');
+        timeSlider.type = 'range';
+        timeSlider.min = 0;
+        timeSlider.max = data.length - 1;
+        timeSlider.step = 1;
+        timeSlider.value = 0;
+        timeSlider.className = 'time-slider';
+        
+        // start time
+        const startLabel = document.createElement('span');
+        startLabel.className = 'start-label';
+        startLabel.textContent = new Date(data[0].time).toLocaleString();
+        
+        // end time
+        const endLabel = document.createElement('span');
+        endLabel.className = 'end-label';
+        endLabel.textContent = new Date(data[data.length - 1].time).toLocaleString();
+        
+        // init labels container
+        const timeLabelsContainer = document.createElement('div');
+        timeLabelsContainer.className = 'label-container';
+        
+        // appending to containers
+        timeSliderContainer.appendChild(timeSlider);
+        timeLabelsContainer.appendChild(startLabel);
+        timeLabelsContainer.appendChild(endLabel);
+        
+        const mapGraph = document.getElementById('mapGraph');
+        // inserting slider container
+        mapGraph.parentNode.insertBefore(timeSliderContainer, mapGraph.nextSibling); // under map
+        // inserting labels container
+        timeSliderContainer.parentNode.insertBefore(timeLabelsContainer, timeSliderContainer.nextSibling); // under timeslider
+        
+        // when resizing
+        window.addEventListener('resize', function() {
+            timeSlider.style.width = timeSliderContainer.offsetWidth + 'px';
+            timeLabelsContainer.style.width = timeSlider.offsetWidth + 'px';
+        });
+        
+        // default values
+        timeSlider.style.width = timeSliderContainer.offsetWidth + 'px';
+        timeLabelsContainer.style.width = timeSlider.offsetWidth + 'px';
+        
+        // last date digit under the end of slider
+        endLabel.style.position = 'absolute';
+        endLabel.style.right = endLabel.offsetWidth + 'px';
+
+        const currentDateBubble = document.createElement('div');
+        currentDateBubble.className = 'current-date-bubble';
+        timeSliderContainer.appendChild(currentDateBubble);
+        currentDateBubble.style.display = 'none'; // invisible until slider changed
+
+        timeSlider.addEventListener('input', function() {
+            currentDateBubble.style.display = 'block';
+            const index = parseInt(this.value);
+            const coordinates = [data[index].longitude, data[index].latitude];
+            const point = ol.proj.fromLonLat(coordinates);
+            map.getView().setCenter(point);
+            
+            const currentDate = new Date(data[index].time).toLocaleString();
+            
+            currentDateBubble.textContent = currentDate;
+
+            selectedFeature = features[index];
+            layer.changed(); // updating layers
+        });
+    }
+}
